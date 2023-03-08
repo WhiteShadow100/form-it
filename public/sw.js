e9f0b091c41bd98d3adbaaf8ac74eb686e65185c
WhiteShadow100 <jmaharjan4@gmail.com>
2023-03-08 22:17:58 +0545

    fixing

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,33 @@
-// // caching required data
-// self.addEventListener('install', function(event){   
-//     event.waitUntil(
-//         caches.open('static')
-//         .then((cache) =>  {
-//             cache.addAll([
-//                 '/',
-//                 '/index.html',
-//                 '/static/js/main.chunk.js',
-//                 '/static/js/0.chunk.js',
-//                 '/static/js/bundle.js', 
-//                 '/page1',   
-//                 '/page2',
-//             ])
-//         })
-//     );
-// })
+// caching required data
+self.addEventListener('active', function(event){  
+        console.log("Datataad")
+    }
+)
+self.addEventListener('install', function(event){   
+    event.waitUntil(
+        caches.open('static')
+        .then((cache) =>  {
+            alert("Olla")
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/static/js/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js', 
+                '/page1',   
+                '/page2',
+            ])
+        })
+    );
+})
 
-// self.addEventListener('fetch', function(event){
-//     event.respondWith(
-//         caches.match(event.request)
-//         .then(function(response){
-//             if(response){
-//                 return response
-//             }
-//         })
-//     )
-// })
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            if(response){
+                return response
+            }
+        })
+    )
+})
